872dade605f44df98b8ff1f8fcc565999c70a9d6
celalcankaya <cckaya08@gmail.com>
2019-05-04 20:55:19 +0300

    Now automatically connects to ESP on MainScreen

--- app/src/main/java/com/example/proje/MainScreen.java
+++ app/src/main/java/com/example/proje/MainScreen.java
@@ -1,12 +1,31 @@
 package com.example.proje;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainScreen extends MenuBar {
 
     int steps = 0;
+    String address="3C:71:BF:AA:DB:F2";
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
 
 
     @Override
@@ -15,6 +34,8 @@ public class MainScreen extends MenuBar {
         setContentView(R.layout.main_screen);
         super.menuBar();
         final TextView sCount = (TextView) findViewById(R.id.stepCounter);
+        new BTbaglan().execute();
+
         // Suanki adım sayısını almamız lazım.
         Thread t = new Thread() {
             @Override
@@ -35,6 +56,55 @@ public class MainScreen extends MenuBar {
             }
         };
         t.start();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class BTbaglan extends AsyncTask<Void, Void, Void> {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(MainScreen.this , "Baglanıyor...", "Lütfen Bekleyin");
+        }
+
+        // https://gelecegiyazanlar.turkcell.com.tr/konu/android/egitim/android-301/asynctask
+        @Override
+        protected Void doInBackground(Void... devices) {
+            try {
+
+                if (btSocket == null || !isBtConnected) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice cihaz = myBluetooth.getRemoteDevice(address);
+                    System.out.println(address);
+                    btSocket = cihaz.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();
+
+
+
+
+                }
+            } catch (IOException e) {
+                ConnectSuccess = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (!ConnectSuccess) {
+                // msg("Baglantı Hatası, Lütfen Tekrar Deneyin");
+                Toast.makeText(getApplicationContext(), "Bağlantı Hatası Tekrar Deneyin", Toast.LENGTH_SHORT).show();
+            } else {
+                //   msg("Baglantı Basarılı");
+                Toast.makeText(getApplicationContext(), "Bağlantı Başarılı", Toast.LENGTH_SHORT).show();
+
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+
     }
 }
 
