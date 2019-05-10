de411427a701bfbc7f7e5c40d1f1cddbf727aa42
Alperen ERDEM <alperenerealist@gmail.com>
2019-05-10 20:33:29 +0300

    Trying Pedometer v1

--- app/src/main/java/com/example/proje/MainScreen.java
+++ app/src/main/java/com/example/proje/MainScreen.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class MainScreen extends MenuBar {
@@ -19,6 +20,13 @@ public class MainScreen extends MenuBar {
     int[] x=new int[3];
     int count=0;
     int flag=0;
+    int zmax=0;
+    int zmin=0;
+    int zmaxNorm=0;
+    int zminNorm=0;
+    int ymax=0;
+    int ymin=0;
+    int CountX=0;
     Thread thread1;
     ImageView isConnectedImage;
 
@@ -47,8 +55,12 @@ public class MainScreen extends MenuBar {
             @Override
             public void run()
             {
+                ArrayList arrList=new ArrayList<Integer>();
                 if(connection.btSocket!=null) {
                     InputStream inputStream = null;
+                    int avg=0;
+                    int sum=0;
+                    int xCount=0;
                     try {
                         inputStream = connection.btSocket.getInputStream();
                     } catch (IOException e) {
@@ -58,57 +70,73 @@ public class MainScreen extends MenuBar {
                         try {
                             int ch;
                             ch=-1;
+                            int zOld=0;
                             while (true) {
-                                if(connection.btSocket!=null) {
-                                    ch = inputStream.read();
-                                }
-                                // System.out.println(ch);
-                                if (ch == 44) {
-                                    x[count]=Integer.parseInt(a);
-                                    System.out.println(a);
-                                    count++;
-                                    if(count==3)
-                                        count=0;
-                                    a="0";
-                                }
-                                else if(ch == 45)
-                                   a="-"+a;
-                                else if(ch == 65)
-                                    break;
-                                else {
-
-                                    if (ch == 46) {
-                                        count=0;
-                                        bpm=Integer.parseInt(a);
-                                        // System.out.println(a);
-                                        length =  Math.sqrt(x[0] * x[0] + x[1] * x[1] + x[2] * x[2]);
-                                        if (flag==0&&x[2]>9000||x[2]<3000) {
-                                            stepcount += 1;
-                                            flag =1;
-                                        }
-                                        else if(x[2]<7500&&x[2]>5000){
-                                            flag=0;
-                                        }
-                                        System.out.println(stepcount);
-                                        break;
+                                    if (connection.btSocket != null) {
+                                        ch = inputStream.read();
                                     }
-                                    if(ch>=48&&ch<=57)
-                                        a = a + Character.toString((char) ch);
-                                }
+                                    if (ch == 44) {
+                                        x[count] = Integer.parseInt(a);
+                                        //System.out.println(a);
+                                        count++;
+                                        if (count == 3) {
+                                            arrList.add(x[2]);
+                                            count = 0;
+                                        }
+                                        a = "0";
+                                    } else if (ch == 45)
+                                        a = "-" + a;
+                                    else if (ch == 65)
+                                        break;
+                                    else {
+                                        if (ch == 46) {
+                                            count = 0;
+                                            bpm = Integer.parseInt(a);
+                                            System.out.println(x[2]+" yy "+x[1] + " x " +x[0]);
+                                            //System.out.println(x[1]);
+                                           /* if(CountX<2){
+                                                CountX++;
+                                                if(CountX==2){
+                                                    int avg=0;
+                                                    int total=0;
+                                                    for(int i = 0; i < arrList.size(); i++)
+                                                    {
+                                                        total = total + (int)arrList.get(i);
+                                                        avg = total / arrList.size();
+                                                    }
+                                                    System.out.println("AVG BU KARDEŞİM"+avg);
+
+                                                }
+                                            }*/
+                                            zmaxNorm=6600+600;
+                                            zminNorm=6600-600;
+                                            zmax=6600+1200;
+                                            zmin=6600-1200;
+                                            if (flag == 0 && ((x[2] > zmax || x[2] < zmin)||(zOld!=0&&x[2]-zOld>3000))) {
+                                                stepcount += 1;
+                                                zOld=x[2];
+                                                System.out.println(stepcount);
+                                                flag = 1;
+                                            } else if (x[2] < zmaxNorm && x[2] > zminNorm) {
+                                                flag = 0;
+                                            }
+                                            break;
+                                        }
+                                        if (ch >= 48 && ch <= 57) {
+                                            a = a + Character.toString((char) ch);
+                                        }
+                                    }
+                                if (ch == 65)
+                                    break;
+
                             }
-
-                            try {
-                                // text.setText(a);
-                                Thread.sleep(100);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            if(ch == 65)
-                                break;
-                            a = "0";
-
-
                         } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            // text.setText(a);
+                            Thread.sleep(30);
+                        } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
